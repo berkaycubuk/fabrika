79803e70e81d9adba9d9715d58f4562f2379b20c
Berkay Cubuk <berkay@berkaycubuk.com>
2026-06-03 19:03:40 +0300

    web: add default anonymous avatar asset
    Co-authored-by: fabrika <fabrika@berkaycubuk.com>

---
+++ web/src/avatar.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_AVATAR =
+  "data:image/svg+xml;utf8,<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 64 64\"><circle cx=\"32\" cy=\"16\" r=\"11\" fill=\"%23a0a0a0\"/><path d=\"M14 56V46Q14 34 22 33Q26 38 32 38Q38 38 42 33Q50 34 50 46V56Z\" fill=\"%23a0a0a0\"/></svg>";
